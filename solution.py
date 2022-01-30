0abf302930d9dca60d22897d46495bac97bf81ac
nishanttayal14 <98676964+nishanttayal14@users.noreply.github.com>
2022-01-30 09:53:41 +0530

    Assignment 1

--- solution.py
+++ solution.py
@@ -7,7 +7,7 @@ def welcome_assignment_answers(question):
     #Another way to implement is using a "case" statements similar to C.
     answer = None
     if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-        answer == "mtls"
+        answer = "mtls"
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
@@ -21,9 +21,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 1
+        answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 2
+        answer = 4
     return(answer)
 # Complete all the questions.
 
